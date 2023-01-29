d18d1af48e517afd1e0cf1bc52331247fe2e8b50
Gabriel Sanches <gabrielsanches1@outlook.com>
2023-01-29 15:00:09 -0300

    Implementing possible moves of king

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -35,12 +35,12 @@ public class Rook extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		// left
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
 		
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
+			p.setRow(p.getRow() + 1);
 		}
 		
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
@@ -59,12 +59,12 @@ public class Rook extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		// below
-		p.setValues(position.getRow() + 1, position.getColumn());
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
 		
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1);
+			p.setColumn(p.getColumn() - 1);
 		}
 		
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
